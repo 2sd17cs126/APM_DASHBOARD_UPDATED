61e688d31465e467b5a8ef3d8e21abc90ddda3c7
Salman Ismail <Salman.Ismail@evryindia.in>
2022-10-04 12:08:45 +0530

    first version of code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { PerformanceComponent } from './performance/performance.component';
+const routes: Routes = [
+  {path:'performance',component:PerformanceComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    MatTabsModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+  
+ }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'apm_dashboard';
+  links=["performance","Automation","About"]
 }
