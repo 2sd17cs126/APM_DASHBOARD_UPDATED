9df8e56dc852214249c42cd1366774cbb0bd4254
Salman Ismail <Salman.Ismail@evryindia.in>
2022-10-27 11:22:55 +0530

    startinf work of apm done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import { RouterModule, Routes } from '@angular/router';
 import { PerformanceComponent } from './performance/performance.component';
+import { AutomationComponent } from './automation/automation.component';
 const routes: Routes = [
-  {path:'performance',component:PerformanceComponent}
+  {path:'performance',component:PerformanceComponent},
+  {path:'automation',component:AutomationComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { PerformanceComponent } from './performance/performance.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AutomationComponent } from './automation/automation.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PerformanceComponent
+    PerformanceComponent,
+    AutomationComponent
   ],
   imports: [
     BrowserModule,
